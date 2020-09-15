a96ba1d46b49c4a12868d0e6162160fb1d570065
Harshal Trivedi <harshaltrivedi16@gmail.com>
2020-09-15 21:18:14 +1000

    function components

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ const app = (props) => {
     //this.state.persons[0].name = "harshal";
     setPersonsState({
       persons: [
-        { name: "Max04", age: 28 },
-        { name: "Max14", age: 27 },
-        { name: "Max2", age: 29 },
+        { name: "Max04", age: 281 },
+        { name: "Max14", age: 271 },
+        { name: "Max23", age: 2911 },
       ],
     });
   };
@@ -32,7 +32,10 @@ const app = (props) => {
       >
         Learning react
       </Person>
-      <Person name={personsState.persons[1].name} age="32" />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[0].age}
+      />
       <Person name="hhm" age="30">
         {" "}
         hakuna{" "}
